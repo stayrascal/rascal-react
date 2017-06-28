19144d5113c54cdda01f85e4c9bd57a04bd17a59
Zhiping Wu <zpwu@thoughtworks.com>
2017-06-28 20:59:21 +0800

    add server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require('ignore-styles')
+require('babel-register')({ ignore: /\/(build|node_modules)\//, presets: ['react-app'] })
+
+const bodyParser = require('body-parser')
+const compression = require('compression')
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+
+//Support Gzip
+app.use(compression())
+
+//Support post requests with body data (doesn't support multipart, use multer)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Setup logger
+app.use(morgan('combined'))
+
